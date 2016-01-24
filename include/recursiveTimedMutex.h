69946622fea880c2733e40f45e340148819f4b0e
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-01-24 09:57:03 +0100

    FIX: recursiveAquire vector correctly used by the classRecursiveTimedMutex.

--- include/recursiveTimedMutex.h
+++ include/recursiveTimedMutex.h
@@ -43,10 +43,6 @@ public:
 
 	template<typename Rep, typename Period>
 	bool try_lock_for( const std::chrono::duration<Rep, Period>& timeout_duration ) {
-		const auto instanceId = RecursiveMutexTemplate<std::timed_mutex>::instanceId;
-		auto &recursiveAquire = RecursiveMutexTemplate<std::timed_mutex>::recursiveAquire;
-		auto &mutex = RecursiveMutexTemplate<std::timed_mutex>::mutex;
-
 		if (instanceId >= recursiveAquire.size())
 			recursiveAquire.resize(instanceId + 1);
 		const auto locked = (0 == recursiveAquire[instanceId]) ? mutex.try_lock_for(timeout_duration) : true;
@@ -56,10 +52,6 @@ public:
 	}
 	template<typename Clock, typename Duration>
 	bool try_lock_until( const std::chrono::time_point<Clock, Duration>& timeout_time ) {
-		const auto instanceId = RecursiveMutexTemplate<std::timed_mutex>::instanceId;
-		auto &recursiveAquire = RecursiveMutexTemplate<std::timed_mutex>::recursiveAquire;
-		auto &mutex = RecursiveMutexTemplate<std::timed_mutex>::mutex;
-
 		if (instanceId >= recursiveAquire.size())
 			recursiveAquire.resize(instanceId + 1);
 		const auto locked = (0 == recursiveAquire[instanceId]) ? mutex.try_lock_until(timeout_time) : true;
