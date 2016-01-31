bb3ccdd71a189ef1a1add8cacf8e5bbef2ec2b0c
Laurent Van Begin <laurent.vanbegin76@gmail.com>
2016-01-31 18:56:12 +0100

    FIX: add .h files.

---
+++ include/templates/recursiveMutexTemplate.h
@@ -0,0 +1,99 @@
+/* Copyright 2016 Laurent Van Begin
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RECURSIVE_MUTEX_TEMPLATE_H__
+#define RECURSIVE_MUTEX_TEMPLATE_H__
+
+#include <stdint.h>
+#include <atomic>
+#include <vector>
+#include <stdexcept>
+#include <chrono>
+#include <functional>
+
+namespace std_mutex_extra {
+
+class RecursiveMutexTemplate {
+public:
+	RecursiveMutexTemplate() = delete;
+	~RecursiveMutexTemplate() = delete;
+
+	template <typename M>
+	static void lock(unsigned int instanceId, M & mutex) {
+		if (instanceId >= recursiveAquireCounters.size())
+			recursiveAquireCounters.resize(instanceId + 1);
+		if (0 == recursiveAquireCounters[instanceId])
+			mutex.lock();
+		recursiveAquireCounters[instanceId]++;
+	}
+	template <typename M>
+	static void unlock(unsigned int instanceId, M & mutex) {
+		if (instanceId >= recursiveAquireCounters.size() || 0 == recursiveAquireCounters[instanceId])
+			throw std::runtime_error("unlock a non-locked lock.");
+		recursiveAquireCounters[instanceId]--;
+		if (0 == recursiveAquireCounters[instanceId])
+			mutex.unlock();
+	}
+	template <typename M>
+	static bool try_lock(unsigned int instanceId, M &mutex) { return nonRecursiveTryLock<M>(instanceId, mutex, [](M &mutex) {return mutex.try_lock(); } ); }
+
+	template<typename M, typename Rep, typename Period>
+	static bool try_lock_for(unsigned int instanceId, M &mutex, const std::chrono::duration<Rep, Period>& timeout_duration ) {
+		const auto &TryLockFunction = [timeout_duration](M &mutex) { return mutex.try_lock_for(timeout_duration); };
+		return RecursiveMutexTemplate::nonRecursiveTryLock<M>(instanceId, mutex, TryLockFunction);
+	}
+	template<typename M, typename Clock, typename Duration>
+	static bool try_lock_until(unsigned int instanceId, M &mutex, const std::chrono::time_point<Clock, Duration>& timeout_time ) {
+		const auto &TryLockFunction = [timeout_time](M &mutex) { return mutex.try_lock_until(timeout_time); };
+		return RecursiveMutexTemplate::nonRecursiveTryLock<M>(instanceId, mutex, TryLockFunction);
+	}
+	static unsigned int newId() {
+		static std::atomic<unsigned int> nbInstances { 0 };
+		return nbInstances++;
+	}
+protected:
+	static thread_local std::vector<uint_fast16_t> recursiveAquireCounters;
+
+	template <typename M>
+	static bool nonRecursiveTryLock(unsigned int instanceId, M &mutex, std::function<bool(M &mutex)> tryLockFunction)
+	{
+		if (instanceId >= recursiveAquireCounters.size())
+			recursiveAquireCounters.resize(instanceId + 1);
+		const auto locked = (0 == recursiveAquireCounters[instanceId]) ? tryLockFunction(mutex) : true;
+		if (locked)
+			recursiveAquireCounters[instanceId]++;
+		return locked;
+	}
+};
+
+}
+
+
+
+#endif
---
+++ include/templates/recursiveSharedMutexTemplate.h
@@ -0,0 +1,91 @@
+/* Copyright 2016 Laurent Van Begin
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RECURSIVE_SHARED_MUTEX_TEMPLATE_H__
+#define RECURSIVE_SHARED_MUTEX_TEMPLATE_H__
+
+#include <sharedMutex.h>
+#include <templates/recursiveMutexTemplate.h>
+
+namespace std_mutex_extra {
+
+class RecursiveSharedMutexTemplate : public  RecursiveMutexTemplate {
+public:
+	template <typename M>
+	static void lock_shared(unsigned int instanceId, M &mutex) {
+		const auto &tryLockFunc = [](M &mutex) { mutex.lock_shared(); return true; };
+		tryLockShared<M>(instanceId, mutex, tryLockFunc);
+	}
+	template <typename M>
+	static void unlock_shared(unsigned int instanceId, M &mutex) {
+		if (instanceId >= recursiveSharedAquire.size() || 0 == recursiveSharedAquire[instanceId])
+			throw std::runtime_error("unlock a non-locked lock.");
+		recursiveSharedAquire[instanceId]--;
+		if (0 == recursiveSharedAquire[instanceId])
+			mutex.unlock_shared();
+	}
+	template <typename M>
+	static bool try_lock_shared(unsigned int instanceId, M &mutex) {
+		const auto &tryLockFunc = [](M &mutex) { return mutex.try_lock_shared(); };
+		return tryLockShared<M>(instanceId, mutex, tryLockFunc);
+	}
+	template<typename M, typename Rep, typename Period>
+	static bool try_lock_for_shared(unsigned int instanceId, M &mutex, const std::chrono::duration<Rep, Period>& timeout_duration ) {
+		const auto &tryLockFunc = [timeout_duration](M &mutex) { return mutex.try_lock_for_shared(timeout_duration); };
+		return tryLockShared<M>(instanceId, mutex, tryLockFunc);
+	}
+	template<typename M, typename Clock, typename Duration>
+	static bool try_lock_until_shared(unsigned int instanceId, M &mutex, const std::chrono::time_point<Clock, Duration>& timeout_time ) {
+		const auto &tryLockFunc = [timeout_time](M &mutex) { return mutex.try_lock_until_shared(timeout_time); };
+		return tryLockShared<M>(instanceId, mutex, tryLockFunc);
+	}
+private:
+	const unsigned int id;
+	SharedMutex mutex;
+	static thread_local std::vector<uint_fast16_t> recursiveSharedAquire;
+
+	template <typename M>
+	static bool tryLockShared(unsigned int instanceId, M &mutex, std::function<bool(M &mutex)> tryLockFunction) {
+		if (instanceId >= recursiveSharedAquire.size())
+			recursiveSharedAquire.resize(instanceId + 1);
+		const auto locked = notAlreadyAquiredShared(instanceId, recursiveSharedAquire, recursiveAquireCounters) ? tryLockFunction(mutex) : true;
+		if (locked)
+			recursiveSharedAquire[instanceId]++;
+		return locked;
+	}
+
+	static bool notAlreadyAquiredShared(unsigned int instanceId, std::vector<uint_fast16_t> &recursiveSharedAquire,
+													const std::vector<uint_fast16_t> &recursiveAquire) {
+		return (0 == recursiveSharedAquire[instanceId] &&
+				((instanceId >= recursiveAquire.size()) || 0 == recursiveAquire[instanceId]));
+	}
+};
+
+}
+#endif
